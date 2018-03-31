b89cb93b561ee1f51ee18de0d4731811ada2c06c
Chris Natter <cnatter@duosecurity.com>
2018-03-30 22:50:25 -0400

    UA

--- ua_parser/user_agent_parser_test.py
+++ ua_parser/user_agent_parser_test.py
@@ -145,11 +145,11 @@ class ParseTest(unittest.TestCase):
             result = {}
             result = user_agent_parser.ParseUserAgent(user_agent_string, **kwds)
             self.assertEqual(
-                result, expected,
-                u'UA: {0}\n expected<{1}, {2}, {3}, {4}> != actual<{5}, {6}, {7}, {8}>'.format(
-                    user_agent_string,
-                    expected['family'], expected['major'], expected['minor'], expected['patch'],
-                    result['family'].encode('utf-8'), result['major'], result['minor'], result['patch']))
+                result, expected)
+#                u'UA: {0}\n expected<{1}, {2}, {3}, {4}> != actual<{5}, {6}, {7}, {8}>'.format(
+#                    user_agent_string,
+#                    expected['family'], expected['major'], expected['minor'], expected['patch'],
+#                    result['family'].encode('utf-8'), result['major'], result['minor'], result['patch']))
 
     def runOSTestsFromYAML(self, file_name):
         yamlFile = open(os.path.join(TEST_RESOURCES_DIR, file_name))
